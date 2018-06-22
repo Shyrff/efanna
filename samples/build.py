cd20697d8dfb19374da7ea297ca6a9f4b3f37300
Maxim Ryndin <mxrynd@ispras.ru>
2018-06-22 13:54:35 +0300

    added search module

--- samples/build.py
+++ samples/build.py
@@ -6,4 +6,11 @@ module1 = Extension('builder',
                     extra_link_args=['-lgomp', '-lboost_timer', '-lboost_chrono', '-lboost_system'],
                     sources = ['builder.cc'])
 
+module2 = Extension('searcher',
+                    include_dirs=['/home/maxim/PycharmProjects/EFANNAbinding/', '/home/maxim/PycharmProjects/EFANNAbinding/general/', '/home/maxim/PycharmProjects/EFANNAbinding/algorithm/'],
+                    extra_compile_args=['-std=c++11', '-march=native','-fopenmp'],
+                    extra_link_args=['-lgomp', '-lboost_timer', '-lboost_chrono', '-lboost_system'],
+                    sources = ['searcher.cc'])
+
 setup(name = 'builder', version = 0.1, description = 'builder for efannapy', ext_modules = [module1])
+setup(name = 'searcher', version = 0.1, description = 'searcher for efannapy', ext_modules = [module2])
---
+++ samples/searcher.cc
@@ -0,0 +1,79 @@
+#include <Python.h>
+
+#include <efanna.hpp>
+#include <iostream>
+#include <fstream>
+#include <ctime>
+#include <malloc.h>
+#include <boost/timer/timer.hpp>
+
+using namespace boost;
+using namespace efanna;
+using namespace std;
+
+void load_data(char* filename, float*& data, size_t& num,int& dim){// load data with sift10K pattern
+  	ifstream in(filename);
+	data = (float*)malloc(num * dim * sizeof(float));
+	int i = 0;
+	std::string str;
+	int lines = 0;
+	while(std::getline(in, str)){
+		istringstream iss(str);
+		do{
+        	string subs;
+        	iss >> subs;
+			float t = strtof((subs).c_str(),0);
+    		if(subs == ""){
+				break;
+			}else{
+				data[i] = t;
+				i++;
+			}
+    	}while(iss);
+		lines ++;
+	}
+  	in.close();
+}
+
+static PyObject * searcher_search(PyObject *self, PyObject *args){
+    char *indata, *intrees, *ingraph, *inquery, *fout, *notr, *it, *psf, *ef, *nn;
+    PyArg_ParseTuple(args, "ssssssssss", &indata, &intrees, &ingraph, &inquery, &fout, &notr, &it, &psf, &ef, &nn);
+
+    float* data_load = NULL;
+    float* query_load = NULL;
+    size_t points_num = 1000000;
+    int dim = 100;
+    load_data(indata, data_load, points_num,dim);
+    size_t q_num = 100;
+    int qdim = 100;
+    load_data(inquery, query_load, q_num,qdim);
+    Matrix<float> dataset(points_num,dim,data_load);
+    Matrix<float> query(q_num,qdim,query_load);
+
+    FIndex<float> index(dataset, new L2Distance<float>(), efanna::KDTreeUbIndexParams(true, 8 ,8 ,10,25,30,10));
+    index.loadTrees(intrees);
+    index.loadGraph(ingraph);
+
+    int search_trees = atoi(notr);
+    int search_epoc = atoi(it);
+    int poolsz = atoi(psf);
+    int search_extend = atoi(ef);
+    int search_method = 0;
+    index.setSearchParams(search_epoc, poolsz, search_extend, search_trees, search_method);
+    boost::timer::auto_cpu_timer timer;
+    index.knnSearch(atoi(nn)/*query nn*/,query);
+    cout<<timer.elapsed().wall / 1e9<<endl;
+
+    index.saveResults(fout);
+    PyObject * ret = PyLong_FromLong(0);
+    return ret;
+}
+
+static PyMethodDef MyMethods[] = {
+    {"search",  searcher_search, METH_VARARGS, "searches"},
+    {NULL, NULL, 0, NULL}        /* Sentinel */
+};
+
+PyMODINIT_FUNC initbuilder(void){
+    (void) Py_InitModule("searcher", MyMethods);
+}
